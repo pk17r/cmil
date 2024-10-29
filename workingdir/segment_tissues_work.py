e7b4ff22359d07982b7e17a6fb97d5c118b595ab
Prashant Kumar <prash.k20@gmail.com>
2024-10-29 01:24:42 -0700

    Segmentation using Banding

--- workingdir/segment_tissues_work.py
+++ workingdir/segment_tissues_work.py
@@ -70,78 +70,180 @@ plt.savefig(filename)
 plt.close()
 del fig
 
-# smaller lumma band
-lumma_band = lumma_channel[3600:3800,:]
-filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Band.png")
-cv2.imwrite(filename, lumma_band, [cv2.IMWRITE_PNG_COMPRESSION , 0])
 
 # Histogram
-fig = plt.hist(lumma_band, bins=10) 
+fig = plt.hist(lumma_channel, bins=25) 
 plt.xlabel('Value')
 plt.ylabel('Frequency')
-plt.title('lumma_band Histogram')
-filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Band Histogram.png")
+plt.title('lumma_channel Histogram')
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Histogram 25.png")
 plt.savefig(filename)
 plt.show()
 
-# decimate image into 10 bands
-lumma_band_decimated = lumma_band/25
-lumma_band_decimated = (np.floor(lumma_band/25)).astype(np.uint8)
-imshow(lumma_band_decimated, 'lumma_band_decimated')
-filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Band Decimated.png")
-cv2.imwrite(filename, lumma_band_decimated * 25, [cv2.IMWRITE_PNG_COMPRESSION , 0])
 
-print("lumma_band_decimated range = [" + str(np.min(lumma_band_decimated)) + "," + str(np.max(lumma_band_decimated)) + "]")
+# define binning in image
+bins = 20
+divisor = (np.floor(255 / bins).astype(np.uint8))
 
-imshow(lumma_band_decimated == 6, 'lumma_band_decimated')
+# decimate image into bins bands
+lumma_bands = (np.floor(lumma_channel/divisor)).astype(np.uint8)
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Bands " + str(bins) + ".png")
+cv2.imwrite(filename, lumma_bands * divisor, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+imshow(lumma_bands, "lumma_bands " + str(bins))
 
 # figure to show different decimated values
-fig, ax_arr = plt.subplots(5, 2, sharex=True, sharey=True, figsize=(20, 12))
-fig.suptitle('Lumma Band Decimated range=[' + str(np.min(lumma_band_decimated)) + "," + str(np.max(lumma_band_decimated)) + "]", fontsize = 25)
-row = 0
-col = 0
-for decimated_band_i in range(1,11):
-    print("decimated_band_i = " + str(decimated_band_i) + "  row = " + str(row) + "  col = " + str(col))
-    if decimated_band_i < 10:
-        ax_arr[row, col].set_title("value <= " + str(decimated_band_i), fontsize = 20)
-    else:
-        ax_arr[row, col].set_title("value <= " + str(decimated_band_i) + " (all true)", fontsize = 20)
-    ax_arr[row, col].set_axis_off()
-    ax_arr[row, col].imshow(lumma_band_decimated <= decimated_band_i)
-    row = row +1
-    if row == 5:
-        row = 0
-        col = 1
+fig, ax_arr = plt.subplots(2, int(bins/2), sharex=True, sharey=True, figsize=(20, 12))
+fig.suptitle("Lumma Bands " + str(bins) + " Bins Representation", fontsize = 25)
+row=0
+col=0
+for band_i in range(0,bins):
+    print("band_i = " + str(band_i))
+    ax_arr[row,col].set_title("value == " + str(band_i + 1), fontsize = 20)
+    ax_arr[row,col].set_axis_off()
+    ax_arr[row,col].imshow(lumma_bands == band_i + 1)
+    col=col+1
+    if col == int(bins/2):
+        row = 1
+        col = 0
 
 plt.tight_layout()
-filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Band Decimated Values 1.png")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Bands " + str(bins) + " Representation.png")
 plt.savefig(filename)
 plt.show()
 plt.close()
 del fig
 
 
-# figure to show different decimated values
-fig, ax_arr = plt.subplots(5, 2, sharex=True, sharey=True, figsize=(20, 12))
-fig.suptitle('Lumma Band Decimated range=[' + str(np.min(lumma_band_decimated)) + "," + str(np.max(lumma_band_decimated)) + "]", fontsize = 25)
-row = 0
-col = 0
-for decimated_band_i in range(1,11):
-    print("decimated_band_i = " + str(decimated_band_i) + "  row = " + str(row) + "  col = " + str(col))
-    ax_arr[row, col].set_title("value == " + str(decimated_band_i), fontsize = 20)
-    ax_arr[row, col].set_axis_off()
-    ax_arr[row, col].imshow(lumma_band_decimated == decimated_band_i)
-    row = row +1
-    if row == 5:
-        row = 0
-        col = 1
+# find band with most number of pixels
 
-plt.tight_layout()
-filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Band Decimated Values 2.png")
-plt.savefig(filename)
-plt.show()
-plt.close()
-del fig
+most_pixels_band = -1;
+most_pixels = 0
+for band_i in range(0,bins+1):
+    n_pixels = np.count_nonzero(lumma_bands == band_i)
+    print("band = " + str(band_i) + "   n_pixels = " + str(n_pixels))
+    if n_pixels > most_pixels:
+        most_pixels = n_pixels
+        most_pixels_band = band_i
+
+print("\nmost_pixels_band = " + str(most_pixels_band) + "   most_pixels = " + str(most_pixels))
+
+# find background
+
+bg1 = lumma_bands == most_pixels_band
+imshow(bg1, "bg1")
+bg2 = morphology.remove_small_objects(bg1, 5000)
+imshow(bg2, "bg2")
+background = morphology.remove_small_holes(bg2, 10000)
+imshow(background, "background")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Background Mask.png")
+cv2.imwrite(filename, background.astype(np.uint8)*255, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+
+
+# Apply the background mask to image
+bg3 = background.astype(np.uint8) * 255
+background_img = cv2.bitwise_and(img_color,img_color,mask = bg3)
+imshow(background_img, "background_img")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Background Image.png")
+cv2.imwrite(filename, background_img, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+
+
+
+import scipy.ndimage as ndimage
+
+def pixels_within_distance(mask, distance):
+    """Finds pixels within a specified distance from a 2D mask."""
+    
+    # Create a distance map from the mask
+    distance_map = ndimage.distance_transform_edt(np.logical_not(mask))
+    
+    # Find pixels within the specified distance
+    pixels_within = distance_map <= distance
+    
+    return pixels_within
+
+result = pixels_within_distance(background, 20)
+
+
+
+# find stroma
+
+# 2 band prior to background band
+stroma_band = most_pixels_band-1
+stroma1 = lumma_bands == stroma_band
+# add one more band behind
+stroma1 = stroma1 + lumma_bands == stroma_band-1
+imshow(stroma1, "stroma1")
+# remove background pixels from stroma
+stroma2 = stroma1 * np.invert(background)
+imshow(stroma2, "stroma2")
+# remove pixels nearby background pixels from stroma
+stroma3 = stroma2 * np.invert(result)
+imshow(stroma3, "stroma3")
+# dilation
+stroma4 = morphology.dilation(stroma3, morphology.square(10))
+imshow(stroma4, "stroma4")
+# remove small objects
+stroma5 = morphology.remove_small_objects(stroma4, 10000)
+imshow(stroma5, "stroma5")
+# remove small holes
+stroma6 = morphology.remove_small_holes(stroma5, 20000)
+# dilation
+stroma7 = morphology.dilation(stroma6, morphology.square(10))
+# remove small holes
+stroma = morphology.remove_small_holes(stroma7, 20000)
+imshow(stroma, "stroma")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Stroma Mask.png")
+cv2.imwrite(filename, stroma.astype(np.uint8)*255, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+
+# Apply the stroma mask to image
+stroma6 = stroma.astype(np.uint8) * 255
+stroma_img = cv2.bitwise_and(img_color,img_color,mask = stroma6)
+imshow(stroma_img, "stroma_img")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Stroma Image.png")
+cv2.imwrite(filename, stroma_img, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+
+
+# Find Epithelia
+
+# Anything not Background and not Stroma is Epithelia
+# band prior to Stroma band
+epithelia_band = stroma_band-2
+epithelia1 = lumma_bands <= epithelia_band
+imshow(epithelia1, "epithelia1")
+epithelia11 = lumma_bands > most_pixels_band
+epithelia12 = epithelia11 + epithelia1
+imshow(epithelia12, "epithelia1")
+# remove background pixels from stroma
+epithelia2 = epithelia12 * np.invert(background)
+# remove stroma pixels from stroma
+epithelia = epithelia2 * np.invert(stroma)
+imshow(epithelia, "epithelia")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Epithelia Mask.png")
+cv2.imwrite(filename, epithelia.astype(np.uint8)*255, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+
+
+# Apply the epithelia mask to image
+epithelia_m = epithelia.astype(np.uint8) * 255
+epithelia_img = cv2.bitwise_and(img_color,img_color,mask = epithelia_m)
+imshow(epithelia_img, "epithelia_img")
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Epithelia Image.png")
+cv2.imwrite(filename, epithelia_img, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+
+
+
+
+
+
+
+
+
+
+# decimate image into 25 bands
+lumma_bands = (np.floor(lumma_channel/10)).astype(np.uint8)
+filename = os.path.join(segmented_images_dir, patient_id + " img#" + str(sub_img_num) + " Lumma Bands 25.png")
+cv2.imwrite(filename, lumma_bands * 10, [cv2.IMWRITE_PNG_COMPRESSION , 0])
+imshow(lumma_bands, 'lumma_bands')
+
 
 
 # fill in values 0-3 from nearby regions
