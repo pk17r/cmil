db6ae5d9612688832e42cc2350ccfa65b84144fa
Prashant Kumar <prash.k20@gmail.com>
2024-11-10 16:20:30 -0800

    Take Available Mem into account before starting new thread

--- segmentation_algo.py
+++ segmentation_algo.py
@@ -3,37 +3,34 @@
 
 ##       SEGMENTATION ALGORITHM         ##
 
-show_images = 0                                 # to display images in real-time
-save_intermediate_images = 0                    # save R-G-B and Y-Cb-Cr Channels
-input_dir = "data/sheffield_h&e"               # all files in here will be read, expected filenames: <filename>.tif
-#input_dir = "data/liverpool_h&e"
-#input_dir = "data_test"
-output_dir = "extracted/sheffield_h&e"         # epithelia and stroma will be saved here
-#output_dir = "extracted/liverpool_h&e"
-#output_dir = "workingdir/segmented"
-#output_dir = "output_test"
-run_over_all_images = 1                         # to run over all images in 'input_dir'
-overwrite_output = 0                            # to overwrite previous output
-#image_name = "test"             # specific image to run with 'run_over_all_images = 0'
+kShowImages = 0                                 # to display images in real-time
+kSaveIntermediateImages = 0                    # save R-G-B and Y-Cb-Cr Channels
+#kInputDir = "data/sheffield_h&e"               # all files in here will be read, expected filenames: <filename>.tif
+kInputDir = "data/liverpool_h&e"
+#kInputDir = "data_test"
+#kOutputDir = "extracted/sheffield_h&e"         # epithelia and stroma will be saved here
+kOutputDir = "extracted/liverpool_h&e"
+#kOutputDir = "workingdir/segmented"
+#kOutputDir = "output_test"
+kRunOverAllImages = 1                         # to run over all images in 'kInputDir'
+kOverwriteOutput = 0                            # to overwrite previous output
+#image_name = "test"             # specific image to run with 'kRunOverAllImages = 0'
 #image_name = "h2114158 h&e_ROI_2"
 #image_name = "h2114186 h&e_ROI_3"
 image_name = "h2114182 h&e_ROI_3"
 #image_name = "h2114155 h&e_ROI_4"
-save_epithelia_and_stroma = 0                   # to save epithelia and stroma output
+kSaveEpitheliaAndStroma = 0                   # to save epithelia and stroma output
 
 # visualizations
 
-output_visualization_dir = "extracted/sheffield_h&e/visualization"         # output for visual comparison b/w input_img-segmented_stroma-segmented_epithelia
-#output_visualization_dir = "extracted/liverpool_h&e/visualization"
-#output_visualization_dir = output_dir
-#rescale_size = 0.5
-save_bins_representation = 0                    # to save Lumma and Red Chroma Bins for visualization
+#kOutputVisualizationDir = "extracted/sheffield_h&e/visualization"         # output for visual comparison b/w input_img-segmented_stroma-segmented_epithelia
+kOutputVisualizationDir = "extracted/liverpool_h&e/visualization2"
+#kOutputVisualizationDir = kOutputDir
+#kRescaleSize = 0.5
+kSaveBinsRepresentation = 0                    # to save Lumma and Red Chroma Bins for visualization
 
 # computing resources
-max_no_of_threads_to_use = 6
-# Recommended RAM > 27Gb
-# If app gets killed by low memory, reduce max_no_of_threads_to_use, or increase it if you have more RAM and CPU Cores
-# If app gets killed, make 'overwrite_output = 0' to start from where it left last time
+kPercentMinMemoryAvailableToStartNewThread = 70     # if execution gets killed because of low memory, increase this constant. If you have > 32G then you can decrease this
 
 ##########################################
 
@@ -53,18 +50,28 @@ from skimage import color
 from skimage import morphology
 #from skimage import segmentation
 #from skimage import filters
-from skimage.transform import rescale
-import scipy.ndimage as ndimage
+#from skimage.transform import rescale
+#import scipy.ndimage as ndimage
 import gc
-import threading
 import psutil
-import concurrent.futures
 import time
+from bounded_pool_executor import BoundedProcessPoolExecutor   # https://github.com/mowshon/bounded_pool_executor/tree/master
+
+def GetPercentAvailableMemory():
+    # Get the virtual memory statistics
+    mem = psutil.virtual_memory()
+    # Get the available memory
+    #total_memory_gb = mem.total / (1024 * 1024 * 1024)
+    #available_memory_gb = mem.available / (1024 * 1024 * 1024)
+    #print(f"Memory: {available_memory_gb:.1f}GB/{total_memory_gb:.1f}GB")
+    percent_mem_available = int(mem.available / mem.total * 100)
+    print(f"Memory Available = {percent_mem_available}%")
+    return percent_mem_available
 
 # Plot the image
 def imshow(img, title):
-    global show_images
-    if show_images == 0:
+    global kShowImages
+    if kShowImages == 0:
         return
     plt.imshow(img)
     plt.tight_layout()
@@ -73,16 +80,16 @@ def imshow(img, title):
     plt.show(block=True)
     plt.close('all')
 
-def pixels_within_distance(mask, distance):
-    """Finds pixels within a specified distance from a 2D mask."""
-    # Create a distance map from the mask
-    distance_map = ndimage.distance_transform_edt(np.logical_not(mask))
-    # Find pixels within the specified distance
-    pixels_within = distance_map <= distance
-    return pixels_within
+#def pixels_within_distance(mask, distance):
+#    """Finds pixels within a specified distance from a 2D mask."""
+#    # Create a distance map from the mask
+#    distance_map = ndimage.distance_transform_edt(np.logical_not(mask))
+#    # Find pixels within the specified distance
+#    pixels_within = distance_map <= distance
+#    return pixels_within
 
 def show_images_side_by_side(images, image_labels, combined_label):
-    global save_bins_representation, figure_id, image_name, output_visualization_dir, show_images
+    global kSaveBinsRepresentation, figure_id, image_name, kOutputVisualizationDir, kShowImages
     no_of_images = len(images)
     fig = plt.figure(num=figure_id, clear=True, figsize=(20, 12))
     ax_arr = fig.subplots(1, no_of_images, sharex=True, sharey=True)
@@ -94,19 +101,19 @@ def show_images_side_by_side(images, image_labels, combined_label):
         ax_arr[col].imshow(images[img_no])
         col=col+1
     plt.tight_layout()
-    if save_bins_representation:
-        filename = os.path.join(output_visualization_dir, image_name + " " + combined_label + ".png")
+    if kSaveBinsRepresentation:
+        filename = os.path.join(kOutputVisualizationDir, image_name + " " + combined_label + ".png")
         plt.savefig(filename)
         print(filename + " saved")
-    if show_images:
+    if kShowImages:
         plt.show()
 
 def create_binned_representation(img_2d, label, no_of_bins = 20, bins_on_plot = 20, first_bin_on_plot = 0):
-    global save_bins_representation, figure_id, image_name, output_visualization_dir, show_images
+    global kSaveBinsRepresentation, figure_id, image_name, kOutputVisualizationDir, kShowImages
     # decimate lumma image into lumma_bins_n
     img_binned = np.clip((np.floor(img_2d.astype(np.float64) * no_of_bins / 255)).astype(np.uint8), 0, no_of_bins-1)
     ## figure to show different img_2d bins
-    if save_bins_representation:
+    if kSaveBinsRepresentation:
         fig = plt.figure(num=figure_id, clear=True, figsize=(20, 12))
         ax_arr = fig.subplots(2, int(bins_on_plot/2), sharex=True, sharey=True)
         fig.suptitle(image_name + " " + label + " " + str(no_of_bins) + " Bins Representation", fontsize = 25)
@@ -121,10 +128,10 @@ def create_binned_representation(img_2d, label, no_of_bins = 20, bins_on_plot = 
                 row = 1
                 col = 0
         plt.tight_layout()
-        filename = os.path.join(output_visualization_dir, image_name + " " + label + " " + str(no_of_bins) + " Bins Representation.png")
+        filename = os.path.join(kOutputVisualizationDir, image_name + " " + label + " " + str(no_of_bins) + " Bins Representation.png")
         plt.savefig(filename)
         print(filename + " saved")
-        if show_images:
+        if kShowImages:
             plt.show()
     # find bin with most number of pixels
     most_pixels_bin = -1;
@@ -143,11 +150,11 @@ def create_binned_representation(img_2d, label, no_of_bins = 20, bins_on_plot = 
 # input:
 #   file_index = index of file in files to work with
 def segmentation_algo(file_index, image_name = ""):
-    global show_images, save_intermediate_images, input_dir, output_dir, save_epithelia_and_stroma, output_visualization_dir, save_bins_representation, files, overwrite_output, run_over_all_images, total_num_of_images
+    global kShowImages, kSaveIntermediateImages, kInputDir, kOutputDir, kSaveEpitheliaAndStroma, kOutputVisualizationDir, kSaveBinsRepresentation, files, kOverwriteOutput, kRunOverAllImages, total_num_of_images
     
     if file_index != -1:
         f = files[file_index]
-        #if run_over_all_images:
+        #if kRunOverAllImages:
         print("\n*********** CURRENT FILE (" + str(file_index+1) + "/" + str(total_num_of_images) + "): " + f)
         x = f.split(".", 1)
         if x[1] != "tif":
@@ -157,11 +164,11 @@ def segmentation_algo(file_index, image_name = ""):
         image_name = x[0]
         del x
     
-    if overwrite_output == 0 and run_over_all_images:
-        if os.path.exists(os.path.join(output_visualization_dir, image_name + ".png")):
+    if kOverwriteOutput == 0 and kRunOverAllImages:
+        if os.path.exists(os.path.join(kOutputVisualizationDir, image_name + ".png")):
             return
     
-    input_filepath = os.path.join(input_dir, image_name)
+    input_filepath = os.path.join(kInputDir, image_name)
     print(input_filepath + ".tif")
     
     # figure id - reusing figures to save memory
@@ -177,7 +184,7 @@ def segmentation_algo(file_index, image_name = ""):
     #end_time = time.perf_counter_ns()
     #print(f"RGB Channels Runtime: {((end_time - start_time) / 1000)} microseconds")
     
-    if save_bins_representation or show_images:
+    if kSaveBinsRepresentation or kShowImages:
         show_images_side_by_side([img_rgb, img_rgb[:,:,0], img_rgb[:,:,1], img_rgb[:,:,2]], ["RGB", "R", "G", "B"], "RGB Channels")
     
     img_YCrCb = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2YCrCb)
@@ -186,7 +193,7 @@ def segmentation_algo(file_index, image_name = ""):
     #imshow(img_YCrCb, 'img_YCrCb')
     del img_YCrCb
     
-    if save_bins_representation or show_images:
+    if kSaveBinsRepresentation or kShowImages:
         show_images_side_by_side([img_rgb, img_Y, img_Cb, img_Cr], ["RGB", "Y", "Cb", "Cr"], "YCbCr Channels")
     
     #img_Cb_binned, img_Cb_most_pixels_bin = save_binned_representation(img_Cb, "Blue Chroma")
@@ -194,13 +201,13 @@ def segmentation_algo(file_index, image_name = ""):
     
     # YUV
     img_yuv_f = color.rgb2yuv(img_rgb, channel_axis=-1)
-    if save_bins_representation or show_images:
+    if kSaveBinsRepresentation or kShowImages:
         show_images_side_by_side([img_rgb, img_yuv_f[:,:,0], img_yuv_f[:,:,1], img_yuv_f[:,:,2]], ["RGB", "Y", "U", "V"], "YUV Channels")
     #save_binned_representation((np.clip(((img_yuv_f[:,:,1] + 1) / 2 * 255), 0, 255)).astype(np.uint8), "U")
     #save_binned_representation((np.clip(((img_yuv_f[:,:,2] + 1) / 2 * 255), 0, 255)).astype(np.uint8), "V")
     
-    #show_images = 1
-    #save_bins_representation = 1
+    #kShowImages = 1
+    #kSaveBinsRepresentation = 1
     img_u = np.clip(((img_yuv_f[:,:,1] + 1) / 2 * 255), 0, 255)
     imshow(img_u, "img_u_f")
     img_v = np.clip(((img_yuv_f[:,:,2] + 1) / 2 * 255), 0, 255)
@@ -210,7 +217,7 @@ def segmentation_algo(file_index, image_name = ""):
     img_v_expanded = (np.clip(((img_yuv_f[:,:,2] * 4 + 1) / 2 * 255), 0, 255)).astype(np.uint8)
     imshow(img_v_expanded, "img_v_expanded")
     
-    if save_bins_representation or show_images:
+    if kSaveBinsRepresentation or kShowImages:
         create_binned_representation(img_u, "img_u")
         create_binned_representation(img_v, "img_v")
         create_binned_representation(img_u_expanded, "img_u_expanded")
@@ -232,15 +239,15 @@ def segmentation_algo(file_index, image_name = ""):
     background = morphology.remove_small_holes(background, 10000)
     print('background located')
     imshow(background, "background")
-    if save_intermediate_images:
-        filename = os.path.join(output_dir, image_name + " Lumma Background Mask.png")
+    if kSaveIntermediateImages:
+        filename = os.path.join(kOutputDir, image_name + " Lumma Background Mask.png")
         cv2.imwrite(filename, background.astype(np.uint8)*255, [cv2.IMWRITE_PNG_COMPRESSION , 0])
         print(filename + " saved")
     
     ## Apply the background mask to image
     #background_img = cv2.bitwise_and(img_rgb, img_rgb, mask = (background.astype(np.uint8) * 255))
     #imshow(background_img, "background_img")
-    #filename = os.path.join(output_dir, image_name + " Lumma Background Image.png")
+    #filename = os.path.join(kOutputDir, image_name + " Lumma Background Image.png")
     #cv2.imwrite(filename, background_img, [cv2.IMWRITE_PNG_COMPRESSION , 0])
     #del background_img
     
@@ -342,8 +349,8 @@ def segmentation_algo(file_index, image_name = ""):
     epithelia = morphology.remove_small_holes(epithelia, 20000)
     print('epithelia located')
     imshow(epithelia, "epithelia")
-    if save_intermediate_images:
-        filename = os.path.join(output_dir, image_name + " Epithelia Mask.png")
+    if kSaveIntermediateImages:
+        filename = os.path.join(kOutputDir, image_name + " Epithelia Mask.png")
         cv2.imwrite(filename, epithelia.astype(np.uint8)*255, [cv2.IMWRITE_PNG_COMPRESSION , 0])
         print(filename + " saved")
     
@@ -351,8 +358,8 @@ def segmentation_algo(file_index, image_name = ""):
     # Apply the epithelia mask to image
     epithelia_img = cv2.bitwise_and(img_rgb,img_rgb,mask = (epithelia.astype(np.uint8) * 255))
     imshow(epithelia_img, "epithelia_img")
-    if save_epithelia_and_stroma:
-        filename = os.path.join(output_dir, image_name + " Epithelia.png")
+    if kSaveEpitheliaAndStroma:
+        filename = os.path.join(kOutputDir, image_name + " Epithelia.png")
         cv2.imwrite(filename, cv2.cvtColor(epithelia_img, cv2.COLOR_RGB2BGR), [cv2.IMWRITE_PNG_COMPRESSION , 0])
         print(filename + " saved")
     
@@ -374,29 +381,29 @@ def segmentation_algo(file_index, image_name = ""):
     stroma = stroma * np.invert(epithelia)
     print('stroma expanded')
     imshow(stroma, "stroma")
-    if save_intermediate_images:
-        filename = os.path.join(output_dir, image_name + " Stroma Mask.png")
+    if kSaveIntermediateImages:
+        filename = os.path.join(kOutputDir, image_name + " Stroma Mask.png")
         cv2.imwrite(filename, stroma.astype(np.uint8)*255, [cv2.IMWRITE_PNG_COMPRESSION , 0])
         print(filename + " saved")
     
     # Apply the stroma mask to image
     stroma_img = cv2.bitwise_and(img_rgb,img_rgb,mask = (stroma.astype(np.uint8) * 255))
     imshow(stroma_img, "stroma_img")
-    if save_epithelia_and_stroma:
-        filename = os.path.join(output_dir, image_name + " Stroma.png")
+    if kSaveEpitheliaAndStroma:
+        filename = os.path.join(kOutputDir, image_name + " Stroma.png")
         cv2.imwrite(filename, cv2.cvtColor(stroma_img, cv2.COLOR_RGB2BGR), [cv2.IMWRITE_PNG_COMPRESSION , 0])
         print(filename + " saved")
     
     
     # Save a segmentation visualization image
     # Combine the images horizontally
-    #stroma_img_rescaled_f = rescale(stroma_img, rescale_size, anti_aliasing=True, channel_axis=2)
-    #epithelia_img_rescaled_f = rescale(epithelia_img, rescale_size, anti_aliasing=True, channel_axis=2)
-    #img_rgb_rescaled_f = rescale(img_rgb, rescale_size, anti_aliasing=True, channel_axis=2)
+    #stroma_img_rescaled_f = rescale(stroma_img, kRescaleSize, anti_aliasing=True, channel_axis=2)
+    #epithelia_img_rescaled_f = rescale(epithelia_img, kRescaleSize, anti_aliasing=True, channel_axis=2)
+    #img_rgb_rescaled_f = rescale(img_rgb, kRescaleSize, anti_aliasing=True, channel_axis=2)
     white_border_f = np.ones((img_rgb.shape[0], 100, 3), dtype=np.uint8) * 255    # White Border - 3 layer
     combined_image = np.hstack((img_rgb, white_border_f, stroma_img, white_border_f, epithelia_img), dtype=np.uint8)
     imshow(combined_image, 'combined_image')
-    filename = os.path.join(output_visualization_dir, image_name + ".png")
+    filename = os.path.join(kOutputVisualizationDir, image_name + ".png")
     cv2.imwrite(filename, cv2.cvtColor(combined_image, cv2.COLOR_RGB2BGR), [cv2.IMWRITE_PNG_COMPRESSION , 0])
     print(filename + " saved")
     
@@ -408,47 +415,48 @@ def segmentation_algo(file_index, image_name = ""):
 
 
 
-if not os.path.isdir(input_dir):
-    print("input_dir: '" + input_dir + "' directory does not exist! Exiting...")
+if not os.path.isdir(kInputDir):
+    print("kInputDir: '" + kInputDir + "' directory does not exist! Exiting...")
     exit()
 
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
-    print("output_dir: '" + output_dir + "' directory created.")
+if not os.path.isdir(kOutputDir):
+    os.mkdir(kOutputDir)
+    print("kOutputDir: '" + kOutputDir + "' directory created.")
 
-if not os.path.isdir(output_visualization_dir):
-    os.mkdir(output_visualization_dir)
-    print("output_visualization_dir: '" + output_visualization_dir + "' directory created.")
+if not os.path.isdir(kOutputVisualizationDir):
+    os.mkdir(kOutputVisualizationDir)
+    print("kOutputVisualizationDir: '" + kOutputVisualizationDir + "' directory created.")
     
 
 # Files and Folders in Input Dir
-files = os.listdir(input_dir)
+files = os.listdir(kInputDir)
 # Filtering only the files.
-files = [f for f in files if os.path.isfile(input_dir+'/'+f)]
+files = [f for f in files if os.path.isfile(kInputDir+'/'+f)]
 total_num_of_images = len(files)
 print(f"Image Dataset size: {total_num_of_images}")
 
 # Get available cpu cores
-cores = os.cpu_count()
-print("Number of CPU cores:", cores)
-
-no_of_threads_to_use = min(cores - 2, max_no_of_threads_to_use)
+print("Number of CPU cores:", os.cpu_count())
+no_of_threads_to_use = os.cpu_count() - 2
 print("Number of Threads to use:", no_of_threads_to_use)
 
 # Get the virtual memory statistics
-mem = psutil.virtual_memory()
-# Get the available memory
-available_memory_bytes = mem.available
-available_memory_gb = available_memory_bytes / (1024 * 1024 * 1024)
-print(f"Available memory: {available_memory_gb:.2f} GB")
+percent_mem_available = GetPercentAvailableMemory()
 
-if run_over_all_images == 1 and save_bins_representation == 0 and show_images == 0:
+if kRunOverAllImages == 1 and kSaveBinsRepresentation == 0 and kShowImages == 0:
     # use multi threading
-    with concurrent.futures.ThreadPoolExecutor(max_workers=no_of_threads_to_use) as executor:
-        executor.map(segmentation_algo, range(total_num_of_images))
+    with BoundedProcessPoolExecutor(max_workers=no_of_threads_to_use) as worker:
+        for f in range(total_num_of_images):
+            percent_mem_available = GetPercentAvailableMemory()
+            while percent_mem_available < kPercentMinMemoryAvailableToStartNewThread:
+                time.sleep(1)
+                percent_mem_available = GetPercentAvailableMemory()
+            print('#%d Worker initialization' % f)
+            worker.submit(segmentation_algo, f)
+            time.sleep(0.5)
 else:
     # run one by one
-    if run_over_all_images == 1:
+    if kRunOverAllImages == 1:
         for f in range(0, total_num_of_images):
             segmentation_algo(f)
     else:
